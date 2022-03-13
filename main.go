b1afbf239602e708fccb92361f46fc7ddc413381
Gerard Yap <cheekian.yap@aiesec.net>
2022-03-13 08:46:34 +0800

    display methods of a type

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "reflect"
+import (
+	"time"
+)
 
 func main() {
 	// t := reflect.TypeOf(3)  // a reflect.Type
@@ -82,19 +84,21 @@ func main() {
 	// d.Set(reflect.ValueOf(4))
 	// fmt.Println(x) // "4"
 
-	x := 1
-	rx := reflect.ValueOf(&x).Elem()
-	rx.SetInt(2)                     // OK, x = 2
-	rx.Set(reflect.ValueOf(3))       // OK, x = 3
-	rx.SetString("hello")            // panic: string is not assignable to int
-	rx.Set(reflect.ValueOf("hello")) // panic: string is not assignable to int
+	// x := 1
+	// rx := reflect.ValueOf(&x).Elem()
+	// rx.SetInt(2)                     // OK, x = 2
+	// rx.Set(reflect.ValueOf(3))       // OK, x = 3
+	// rx.SetString("hello")            // panic: string is not assignable to int
+	// rx.Set(reflect.ValueOf("hello")) // panic: string is not assignable to int
 
-	var y interface{}
-	ry := reflect.ValueOf(&y).Elem()
-	ry.SetInt(2)                     // panic: SetInt called on interface Value
-	ry.Set(reflect.ValueOf(3))       // OK, y = int(3)
-	ry.SetString("hello")            // panic: SetString called on interface Value
-	ry.Set(reflect.ValueOf("hello")) // OK, y = "hello"
+	// var y interface{}
+	// ry := reflect.ValueOf(&y).Elem()
+	// ry.SetInt(2)                     // panic: SetInt called on interface Value
+	// ry.Set(reflect.ValueOf(3))       // OK, y = int(3)
+	// ry.SetString("hello")            // panic: SetString called on interface Value
+	// ry.Set(reflect.ValueOf("hello")) // OK, y = "hello"
+
+	Methods(time.Hour)
 }
 
 type Movie struct {
---
+++ methods.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+// Print prints the method set of the value x.
+func Methods(x interface{}) {
+	v := reflect.ValueOf(x)
+	t := v.Type()
+	fmt.Printf("type %s\n", t)
+
+	for i := 0; i < v.NumMethod(); i++ {
+		methType := v.Method(i).Type()
+		fmt.Printf("func (%s) %s%s\n", t, t.Method(i).Name,
+			strings.TrimPrefix(methType.String(), "func"))
+	}
+}
